88e185d45dc94e40d952fa2d1f34d569510e8f52
Theosly <theoamagalhaespc@gmail.com>
2024-07-24 21:02:56 -0300

    Update DistribuicoesLambda.java

--- src/entidades/distribuicoes/abstracts/DistribuicoesLambda.java
+++ src/entidades/distribuicoes/abstracts/DistribuicoesLambda.java
@@ -2,4 +2,7 @@ package entidades.distribuicoes.abstracts;
 
 public abstract class DistribuicoesLambda implements Distribuicao {
    Double lambda;
+public Double calcular(Integer x) {
+	return null;
+	}
 }
